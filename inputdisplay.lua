b4d4c3bf0915cdfcb91910072a4110b06cc0f202
theballaam96 <theballaam96@gmail.com>
2018-07-22 16:25:22 +0100

    TAS Support
    If movie is in play mode, the input_array that the display focuses on shifts to the input file of the aforementioned movie

--- inputdisplay.lua
+++ inputdisplay.lua
@@ -5,100 +5,143 @@ controllerView_value = 1;
 		
 -- Input
 function getControllerInput()
-	input_array = joypad.get(controllerView_value);
-		
-	if input_array.A then
+	
+	moviemode = movie.mode();
+	frame = emu.framecount();
+	if moviemode == "PLAY" then
+		input_array = movie.getinput(frame);
+		Buttons = {
+			["A"] = "P"..controllerView_value.." A",
+			["B"] = "P"..controllerView_value.." B",
+			["CU"] = "P"..controllerView_value.." C Up",
+			["CD"] = "P"..controllerView_value.." C Down",
+			["CL"] = "P"..controllerView_value.." C Left",
+			["CR"] = "P"..controllerView_value.." C Right",
+			["DU"] = "P"..controllerView_value.." DPad U",
+			["DD"] = "P"..controllerView_value.." DPad D",
+			["DL"] = "P"..controllerView_value.." DPad L",
+			["DR"] = "P"..controllerView_value.." DPad R",
+			["L"] = "P"..controllerView_value.." L",
+			["R"] = "P"..controllerView_value.." R",
+			["S"] = "P"..controllerView_value.." Start",
+			["X"] = "P"..controllerView_value.." X Axis",
+			["Y"] = "P"..controllerView_value.." Y Axis",
+			["Z"] = "P"..controllerView_value.." Z",			
+		};
+	else
+		input_array = joypad.get(controllerView_value);
+		Buttons = {
+			["A"] = "A",
+			["B"] = "B",
+			["CU"] = "C Up",
+			["CD"] = "C Down",
+			["CL"] = "C Left",
+			["CR"] = "C Right",
+			["DU"] = "DPad U",
+			["DD"] = "DPad D",
+			["DL"] = "DPad L",
+			["DR"] = "DPad R",
+			["L"] = "L",
+			["R"] = "R",
+			["S"] = "Start",
+			["X"] = "X Axis",
+			["Y"] = "Y Axis",
+			["Z"] = "Z",			
+		};
+	end
+	
+	if input_array[Buttons.A] then
 		press_a = 1;
 	else
 		press_a = 0;
 	end
 		
-	if input_array.B then
+	if input_array[Buttons.B] then
 		press_b = 1;
 	else
 		press_b = 0;
 	end
 	
-	if input_array["C Down"] then
+	if input_array[Buttons.CD] then
 		press_cd = 1;
 	else
 		press_cd = 0;
 	end
 	
-	if input_array["C Left"] then
+	if input_array[Buttons.CL] then
 		press_cl = 1;
 	else
 		press_cl = 0;
 	end
 		
-	if input_array["C Right"] then
+	if input_array[Buttons.CR] then
 		press_cr = 1;
 	else
 		press_cr = 0;
 	end
 	
-	if input_array["C Up"] then
+	if input_array[Buttons.CU] then
 		press_cu = 1;
 	else
 		press_cu = 0;
 	end
 		
-	if input_array.Start then
+	if input_array[Buttons.S] then
 		press_s = 1;
 	else
 		press_s = 0;
 	end
 		
-	if input_array["DPad D"] then
+	if input_array[Buttons.DD] then
 		press_dd = 1;
 	else
 		press_dd = 0;
 	end
 		
-	if input_array["DPad L"] then
+	if input_array[Buttons.DL] then
 		press_dl = 1;
 	else
 		press_dl = 0;
 	end
 		
-	if input_array["DPad U"] then
+	if input_array[Buttons.DU] then
 		press_du = 1;
 	else
 		press_du = 0;
 	end
 		
-	if input_array["DPad R"] then
+	if input_array[Buttons.DR] then
 		press_dr = 1;
 	else
 		press_dr = 0;
 	end
 		
-	if input_array.L then
+	if input_array[Buttons.L] then
 		press_l = 1;
 	else
 		press_l = 0;
 	end
 		
-	if input_array.R then
+	if input_array[Buttons.R] then
 		press_r = 1;
 	else
 		press_r = 0;
 	end
 		
-	if input_array.Z then
+	if input_array[Buttons.Z] then
 		press_z = 1;
 	else
 		press_z = 0;
 	end
 		
-	if input_array["X Axis"] ~= nil then
-		ana_x = input_array["X Axis"];
+	if input_array[Buttons.X] ~= nil then
+		ana_x = input_array[Buttons.X];
 	else
 		ana_x = 0;
 	end
 	
-	if input_array["Y Axis"] ~= nil then
-		ana_y = input_array["Y Axis"];
+	if input_array[Buttons.Y] ~= nil then
+		ana_y = input_array[Buttons.Y];
 	else
 		ana_y = 0;
 	end
